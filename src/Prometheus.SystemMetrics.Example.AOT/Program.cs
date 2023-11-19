316558ca8f97c5b487ac7ba6dc4a5bc670a8a688
Daniel Lo Nigro <d@d.sb>
2023-11-18 22:06:08 -0800

    Add AoT example

---
+++ src/Prometheus.SystemMetrics.Example.AOT/Program.cs
@@ -0,0 +1,23 @@
+﻿using Prometheus;
+using Prometheus.SystemMetrics;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+var services = builder.Services;
+services.AddSystemMetrics();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.MapMetrics();
+app.MapGet("/", async context =>
+{
+	context.Response.Redirect("/metrics");
+});
+
+app.Run();
